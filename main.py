32c24a7451c3db8fc56845a23dd34bdca8dcceed
Khashayar Mirsadeghi <mms.khashayar@gmail.com>
2024-06-25 06:24:09 +0330

    chore: add xor

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ logging.basicConfig(level=logging.INFO)
 
 async def main():
     Gate.discover("./custom/")
-    a = Gate.by_name("tristatebuffer")
-    print(await a.get_output(i=0, e=0))
+    a = Gate.by_name("xor")
+    print(await a.get_output(a=1, b=0))
 
 
 asyncio.run(main())
