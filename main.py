a81823056d508ffbabfeb68b87c043c4ca6be110
Khashayar Mirsadeghi <mms.khashayar@gmail.com>
2024-06-28 00:30:14 +0330

    chore: add 3-bit-nand gate

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ logging.basicConfig(level=logging.INFO)
 
 async def main():
     Gate.discover("./custom/")
-    a = Gate.by_name("3b-and")
+    a = Gate.by_name("3b-nand")
 
     print(await a.get_output(a=1, b=1, c=0))
     print(await a.get_output(a=1, b=1, c=1))
