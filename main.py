f66bb99f57acbc33d36c5740b2327e0923e791af
Khashayar Mirsadeghi <mms.khashayar@gmail.com>
2024-06-29 02:35:22 +0330

    chore: add master-slave-s-r-flip-flop

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 import asyncio
 import logging
-from typing import cast
-from core.clock import Clock
-from core.gate import BIT, Gate
+from core.gate import Gate
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.WARNING)
 
 
 async def main():
-    Gate.discover("./custom/")
-    a = Gate.by_name("3b-nand")
+    await Gate.discover("./custom/")
+    a = await Gate.by_name("gated-s-r-latch")
+    print(a.output_values)
+    print(await a.get_output(s=1, r=0, e=0))
+    print(await a.get_output(s=1, r=0, e=1))
+    print(await a.get_output(s=1, r=0, e=0))
 
-    print(await a.get_output(a=1, b=1, c=0))
-    print(await a.get_output(a=1, b=1, c=1))
+    print()
+
+    a = await Gate.by_name("master-slave-s-r-flip-flop")
+    print(a.output_values)
+    print(await a.get_output(s=1, r=0, clk=0))
+    print(await a.get_output(s=1, r=0, clk=1))
+    print(await a.get_output(s=1, r=0, clk=0))
 
 
 asyncio.run(main())
