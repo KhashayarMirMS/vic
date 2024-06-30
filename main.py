d77b15f2503c424a2fc3473a2cd5e7a3f7b1f4e6
Khashayar Mirsadeghi <mms.khashayar@gmail.com>
2024-06-30 22:18:11 +0330

    chore: add 4x1-mux

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ logging.basicConfig(level=logging.WARNING)
 
 async def main():
     await Gate.discover("./custom/")
-    a = await Gate.by_name("4b-or")
+    a = await Gate.by_name("4x1-mux")
 
-    print(await a.get_output(i=[0, 0, 0, 1]))
+    print(await a.get_output(i=[0, 1, 1, 1], s=[0, 0]))
 
 asyncio.run(main())
---
+++ test/test_4x1_mux.py
@@ -0,0 +1,22 @@
+from typing import cast
+import pytest
+
+from core.gate import BIT, Gate
+
+
+def to_bits(n: int) -> list[BIT]:
+    return list(map(lambda c: cast(BIT, int(c)), reversed(f"{n:02b}")))
+
+
+@pytest.mark.asyncio
+async def test_4x1_mux():
+    await Gate.discover("./custom/")
+
+    gate = await Gate.by_name("4x1-mux")
+
+    for x in range(4):
+        i: list[BIT] = [0 if y == x else 1 for y in range(4)]
+        s = to_bits(x)
+
+        output = await gate.get_output(i=i, s=s)
+        assert output == {"y": 0}
